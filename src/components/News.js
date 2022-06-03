111e6ac163d8eb799fd6b90023599cfbe6207fa5
nihirchandila <94970433+nihirchandila@users.noreply.github.com>
2022-06-03 20:05:53 +0530

    initial commit

---
+++ src/components/News.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+export default class news extends Component {
+  render() {
+    return (
+      <>
+            <div class="news-components mt-5 mb-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="card col-md-4">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="/" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                        <div class="card col-md-4">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="/" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                        <div class="card col-md-4">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href="/" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>       
+      </>
+    )
+  }
+}

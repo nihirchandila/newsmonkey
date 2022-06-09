e166f6313701b447d8cb2e3e2da41a2ec5c698ee
nihirchandila <94970433+nihirchandila@users.noreply.github.com>
2022-06-09 13:14:49 +0530

    added loop for news on news page

--- src/components/News.js
+++ src/components/News.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react'
 
-export default class news extends Component {
+export default class News extends Component {
+
   render() {
+      let {title, description, imageUrl}= this.props;
     return (
-      <>
-            <div class="news-components mt-5 mb-5">
-                <div class="container">
-                    <div class="row">
-                        <div class="card col-md-4">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                        </div>
-                        <div class="card col-md-4">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                        </div>
-                        <div class="card col-md-4">
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkKpstx3qklf3nb0uLwQSMmTE_z0XZyyF-qhZpA4u_WA&s" class="card-img-top" alt="..."/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="/" class="btn btn-primary">Go somewhere</a>
-                                </div>
-                        </div>
-                    </div>
+        <>
+            <div className="card col-md-4 mb-5">
+                <img src={imageUrl} style={{height: '250px'}} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p></p>
+                    <p className="card-text">{description}</p>
+                    <a href="/" className="btn btn-primary">Go somewhere</a>
                 </div>
-            </div>       
-      </>
+            </div>
+
+        </>
     )
   }
 }
